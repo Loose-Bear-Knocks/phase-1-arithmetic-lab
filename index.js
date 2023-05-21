26a9015a6c461935716b139143b53526c53dd758
Loose-Bear-Knocks <131018437+Loose-Bear-Knocks@users.noreply.github.com>
2023-05-21 18:23:26 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+
+let random = Math.floor(Math.random()>0);
+
+let num3 = 8
+let num4 = 2
+let mod = (num3/num4)
+
+let max = Math.max(20)
+    
